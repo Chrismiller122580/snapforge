6c1e57489381b7ab65a3b2043b884951fa156b4f
Chrismiller122580 <chrismiller122580@gmail.com>
2026-07-05 17:42:00 -0600

    Add initial Forgeara customizer page placeholder

---
+++ app/customize/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function CustomizePage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">Forgeara - Customize Your Ride</h1>
+      <p>Upload snapped photo or select from garage. Browse options below.</p>
+      {/* Tabs, preview, etc placeholder */}
+      <button>Apply Mod</button>
+    </div>
+  );
+}
